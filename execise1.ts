685cd1b8dd0d0f8cfe738568d685b499f1c47719
DA MAC M1 120 <damacm1120@DAs-MacBook-Air.local>
2023-04-14 11:33:47 +0200

    updated

--- execise1.ts
+++ execise1.ts
@@ -1,3 +1,27 @@
+function poundsToKg(pounds:number): number{
+    return pounds * 0.45359237;
+}
+console.log(poundsToKg(100));
+
+
+function inchesToCentimeters(inches:number): number{
+    return inches * 2.54;
+}
+console.log(inchesToCentimeters(5))
+
+
+function fahrenheitToCelsius(fahrenheit:number):number{
+    return fahrenheit - 32 * 5/9;
+}
+console.log(fahrenheitToCelsius(670));
+
+
+function feetToMeters(feet:number):number{
+    return feet / 3.281;
+}
+console.log(feetToMeters(20));
+
+
 function oddOrEven(value:number):string{
     if (value%2==0){
         return "number is even";
@@ -7,42 +31,24 @@ function oddOrEven(value:number):string{
         return "invalid value or type";
     }
 }
-
 console.log(oddOrEven(5));
 
+
 function fizzBuzz(num: number): string{
-    if (num % 3 == 0){
-        return "Fizz!!";
+    if (num % 3 == 0 && num % 5 == 0 ){
+        return "FizzBuzz!!";
     }else if(num % 5 == 0){
         return "Buzz!!";
     }
-    else if (num%3 == 0 && num%5 == 0)
-    {
-        return "FizzBuzz!!"
+    else if (num % 3 == 0)
+        {
+        return "Fizzy!!"
     }else{
         return "Normal Number"
     }
 }
+console.log(fizzBuzz(10));
 
-console.log(fizzBuzz(5))
 
 
-function poundsToKg(pounds:number): number{
-    return pounds * 0.45359237;
-}
-console.log(poundsToKg(100));
 
-function inchesToCentimeters(inches:number): number{
-    return inches * 2.54;
-}
-console.log(inchesToCentimeters(5))
-
-function fahrenheitToCelsius(fahrenheit:number):number{
-    return fahrenheit - 32 * 5/9;
-}
-console.log(fahrenheitToCelsius(670));
-
-function feetToMeters(feet:number):number{
-    return feet / 3.281;
-}
-console.log(feetToMeters(20));
